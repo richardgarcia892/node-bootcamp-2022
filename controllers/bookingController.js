d8271ac0f5a5f00b41d2ae0760134de8d542192c
Richard Garcia <richardgarcia892@gmail.com>
2023-09-13 16:55:50 -0400

    fix error creating booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -40,22 +40,19 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.line_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
 exports.createBookingCheckoutHandler = catchAsync(async (req, res, next) => {
-  console.log('handler');
   const signature = req.headers['stripe-signature'];
 
   let event;
   try {
     event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
   } catch (err) {
-    console.log('error with event');
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
-  console.log({ event });
   if (event.type === 'checkout.session.completed') createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
